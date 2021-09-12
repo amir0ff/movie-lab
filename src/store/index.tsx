03915141d7ab21d90f09f14fe49caf906d2ad6e8
Amir <ameer157@gmail.com>
2021-09-13 01:10:04 +0300

    feat: add storage browser support check stop state change on initial render

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactElement, ReactNode, useEffect, useReducer } from 'react';
+import React, { createContext, ReactElement, ReactNode, useEffect, useReducer, useRef } from 'react';
 import Reducer from './reducer';
 import { ContextType, GlobalStateInterface } from './types';
 
@@ -8,21 +8,41 @@ import { ContextType, GlobalStateInterface } from './types';
  **/
 export function GlobalStore({ children }: { children: ReactNode }): ReactElement {
   const [globalState, dispatch] = useReducer(Reducer, initializeState());
+  const initialRenderGlobalState = useRef(true);
+  const initialRenderPersistenceType = useRef(true);
 
   useEffect(() => {
     /*
-     populate either sessionStorage or localStorage data from globalState based on
-     persistenceType and purge sessionStorage or localStorage when either is selected
+     populate either sessionStorage or localStorage
+     data from globalState based on persistenceType
     */
+    if (initialRenderGlobalState.current) {
+      initialRenderGlobalState.current = false;
+      return;
+    }
     const getPersistenceType = globalState.persistenceType;
     if (getPersistenceType === 'sessionStorage') {
       sessionStorage.setItem('globalState', JSON.stringify(globalState));
-      localStorage.removeItem('globalState');
     } else if (getPersistenceType === 'localStorage') {
       localStorage.setItem('globalState', JSON.stringify(globalState));
-      sessionStorage.removeItem('globalState');
     }
   }, [globalState]);
+
+  useEffect(() => {
+    /*
+     purge sessionStorage or localStorage when either is selected
+    */
+    if (initialRenderPersistenceType.current) {
+      initialRenderPersistenceType.current = false;
+      return;
+    }
+    const getPersistenceType = globalState.persistenceType;
+    if (getPersistenceType === 'sessionStorage') {
+      localStorage.removeItem('globalState');
+    } else if (getPersistenceType === 'localStorage') {
+      sessionStorage.removeItem('globalState');
+    }
+  }, [globalState.persistenceType]);
 
   return <globalContext.Provider value={{ globalState, dispatch }}>{children}</globalContext.Provider>;
 }
@@ -41,6 +61,12 @@ function initializeState() {
    the order in which the the data is compared is very important;
    first try to populate the state from Storage if not return initialState
   */
+
+  if (typeof (Storage) !== 'undefined') {
+  } else {
+    throw new Error('You need to enable Storage to run this app.');
+  }
+
   const fromLocalStorage = JSON.parse(localStorage.getItem('globalState') as string);
   const fromSessionStorage = JSON.parse(sessionStorage.getItem('globalState') as string);
   return fromSessionStorage || fromLocalStorage || initialState;
